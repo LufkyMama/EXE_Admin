beef457171f140fc24c3031de4a68a19222c8b5b
LufkyMama <163436312+LufkyMama@users.noreply.github.com>
2025-10-02 15:06:52 +0700

    Deploy fix

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const baseURL = import.meta.env.DEV
+  ? import.meta.env.VITE_API_BASE_URL 
+  : "/api";      
+
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
+    baseURL,
     withCredentials: false,
 });
 
